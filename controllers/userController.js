cd14f4a4918ce6693dcfacd9b9b510625468890c
igrzi <igorzimmer01@gmail.com>
2025-04-21 22:59:08 -0300

    feat: Implemented deleteUserById and patchUserById

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,28 +4,32 @@ const pool = require('../db/db');
 const createUser = async (req, res) => {
   const { name } = req.body;
 
+  // Additional check to see if name is always provided
+  if (!name) {
+    return res.status(400).json({ error: 'Name is required' });
+  }
+
   try {
-    // Even if the user already exists, it returns an error, but still increments the id
     const result = await pool.query(
       'INSERT INTO users (name) VALUES ($1) RETURNING *',
       [name]
     );
 
-    if (result.rowCount === 0) {
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    if (error.code === '23505') { // Unique violation error code, name can't be duplicated
       return res.status(409).json({ error: 'User already exists' });
     }
 
-    res.status(201).json(result.rows[0]);
-  } catch (error) {
     console.error('Error creating user:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
-}
+};
 
 // Get all users from db
 const getUsers = async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM users');
+    const result = await pool.query('SELECT * FROM users ORDER BY id');
     res.status(200).json(result.rows);
   } catch (error) {
     console.error('Error fetching users:', error);
@@ -35,7 +39,7 @@ const getUsers = async (req, res) => {
 
 // Get the user by id
 const getUserById = async (req, res) => {
-  const id = req.query.id;
+  const id = req.params.id;
 
   try {
     const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
@@ -51,5 +55,44 @@ const getUserById = async (req, res) => {
   }
 }
 
+// Delete the user by id
+const deleteUserById = async (req, res) => {
+  const id = req.params.id;
 
-module.exports = { getUserById, getUsers, createUser };
+  try {
+    const result = await pool.query('DELETE FROM users WHERE id = $1', [id]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+// Update the user by id
+const patchUserById = async (req, res) => {
+  const id = req.params.id;
+  const { name } = req.body;
+
+  try {
+    const result = await pool.query(
+      'UPDATE users SET name = $1 WHERE id = $2 RETURNING *',
+      [name, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+module.exports = { getUserById, getUsers, createUser, deleteUserById, patchUserById };
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUserById, createUser } = require('../controllers/userController');
+const { getUsers, getUserById, createUser, deleteUserById, patchUserById } = require('../controllers/userController');
 
 // POST /user
 router.post('/create', createUser);
 
 // GET /users
-router.get('/', (req, res) => {
-  if (req.query.id) {
-    return getUserById(req, res);
-  }
-  return getUsers(req, res);
-});
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+
+// DELETE /user/:id
+router.delete('/:id', deleteUserById);
+
+// PATCH /user/:id
+// We use PATCH instead of PUT because we are not updating the entire user object, just the name
+router.patch('/:id', patchUserById);
 
 module.exports = router;
